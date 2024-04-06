c3de0b8ea57419417bd073c1c7bf4cd5db96252d
Aayush259 <aayushk4980@gmail.com>
2024-04-06 11:54:46 +0530

    Added feature to preview result

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ const InputButtons = document.querySelectorAll('.input-button, #delete-btn');
 // Getting output box.
 const OutputBox = document.querySelector('#output-box');
 
+// Getting preview box.
+const PreviewBox = document.getElementById('result-preview');
+
 /*
     This function changes the theme of the app based on the type of theme passed as argument.
 */
@@ -74,13 +77,24 @@ let buttonText = null;
 */
 const Calculate = (exp) => {
 
+    const ArrayOfOperators = [`+`, `-`, `X`, `÷`, `^`];
+
+    // Getting first and last characters of expression.
+    const ExpFirstCharacter = exp[0];
+    const ExpLastCharacter = exp[exp.length - 1];
+
+    // If the first or last character of expression is an operator then remove it from the expression.
+    if (ArrayOfOperators.includes(ExpFirstCharacter)) {
+        exp = exp.slice(1);
+    } else if (ArrayOfOperators.includes(ExpLastCharacter)) {
+        exp = exp.slice(0, -1);
+    }
+
     // Replace the divide, multiply, exponent and pi symbol to actual symbols in javascript.
     exp = exp.replace(/÷/g, '/');
     exp = exp.replace(/X/g, '*');
     exp = exp.replace(/\^/g, '**');
     exp = exp.replace(/π/g, '3.142857142857143');
-
-    console.log(exp);
     
     try {
         // Return the calculated result if there is no error in the expression.
@@ -98,6 +112,23 @@ const Calculate = (exp) => {
 }
 
 /*
+    This function display the preview of the result on screen.
+*/
+const PreviewResult = () => {
+
+    // Getting expression value from OutputBox.
+    expression = OutputBox.value;
+
+    const PreviewResult = Calculate(expression);
+
+    if (PreviewResult !== `Error`) {
+        PreviewBox.innerHTML = PreviewResult;
+    };
+
+    expression = null;
+}
+
+/*
     This function removes the last character from the output box.
 */
 const Backspace = () => {
@@ -109,6 +140,7 @@ const Backspace = () => {
 */
 const Clear = () => {
     OutputBox.value = '';
+    PreviewBox.innerHTML = '';
 }
 
 // Adding event listener to each input button using for each loop.
@@ -121,11 +153,16 @@ InputButtons.forEach(button => {
         // If buttonText is 'x', then remove the last character from the output box.
         if (buttonText === 'x') {
             Backspace();
+
+            // Preview the result.
+            PreviewResult();
+
         }   // If buttonText is '=', then calculate the expression.
         else if (buttonText === '=') {
             expression = OutputBox.value;
             const Result = Calculate(expression);
             OutputBox.value = Result;
+            PreviewBox.innerHTML = '';
             expression = null;
         }   // If buttonText is 'AC' then clear the output box.
         else if (buttonText === 'AC') {
@@ -133,9 +170,15 @@ InputButtons.forEach(button => {
         }   // If buttonText is 'x2' then raised to the power 2.
         else if (buttonText === 'x2') {
             OutputBox.value += '^2';
+
+            // Preview the result.
+            PreviewResult();
         }
         else {
             OutputBox.value += buttonText;
+
+            // Preview the result.
+            PreviewResult();
         }
 
         // Again setting the buttonText to null.
